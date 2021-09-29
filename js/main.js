742cc02ea9361b0199a8328ac189185f626c5704
Mamun Or Rashid <maamunrcd@gmail.com>
2021-09-29 13:23:14 +0600

    Change slider animation effect

--- js/main.js
+++ js/main.js
@@ -6,6 +6,45 @@ $(document).ready(function () {
     $('.carousel').carousel({
         interval: 5000
     })
+
+
+    const myCarousel = document.getElementById("slider-captions");
+    const carouselIndicators = myCarousel.querySelectorAll(
+        ".carousel-indicators button span"
+    );
+    let intervalID;
+
+    const carousel = new bootstrap.Carousel(myCarousel);
+
+    window.addEventListener("load", function () {
+        fillCarouselIndicator(1);
+    });
+
+    myCarousel.addEventListener("slide.bs.carousel", function (e) {
+        let index = e.to;
+        fillCarouselIndicator(++index);
+    });
+
+    function fillCarouselIndicator(index) {
+        let i = 0;
+        for (const carouselIndicator of carouselIndicators) {
+            carouselIndicator.style.width = 0;
+        }
+        clearInterval(intervalID);
+        carousel.pause();
+
+        intervalID = setInterval(function () {
+            i++;
+
+            myCarousel.querySelector(".carousel-indicators .active span").style.width =
+                i + "%";
+
+            if (i >= 100) {
+                // i = 0; -> just in case
+                carousel.next();
+            }
+        }, 50);
+    }
 });
 (function () {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
